c0bbb118e3fbd2f5c74a7748a8443af0108db245
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-07-29 22:32:14 -0400

    Create num.js

---
+++ num.js
@@ -0,0 +1,22 @@
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random()* 2;
+
+  // Only change code above this line
+};
+
+function randomWholeNum() {
+
+  // Only change code below this line
+
+  return Math.floor(Math.random()* 10);
+}
+
+function convertToInteger(str) {
+var num = parseInt(str);
+return num;
+}
+
+convertToInteger("56");
